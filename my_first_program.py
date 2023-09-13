fdc86591763d76e4d14402e2ac818a2938c7c550
Mahkam02 <144849180+Mahkam02@users.noreply.github.com>
2023-09-13 22:05:14 +0300

    Update my_first_program.py

--- my_first_program.py
+++ my_first_program.py
@@ -5,25 +5,25 @@ Created on Fri Sep  1 20:15:22 2023
 @author: Mahkam
 """
 
-#print("Hello world")
-#print(18*5-37)
-#print("hello \"world\" from pythonist")
-#print("""my name is Mahkam 
-#      I'm 21 years old""")
-#print("Mahkam \nKenjaboyev")
-#name="Mahkam"
-#age=21
-#print(name)
-#print(age)
-#print(f"my name is {name}")
-#print("Hello \tworld")
-#print("hello \nworld")
-#name=input("What is yoour name?\n>>>")
-#print("Hello,"+ name.title())
-#temp=36.6
-#aholi_soni=7_594_895_255
-#print(aholi_soni)
-#PI=3.14159
+print("Hello world")
+print(18*5-37)
+print("hello \"world\" from pythonist")
+print("""my name is Mahkam 
+      I'm 21 years old""")
+print("Mahkam \nKenjaboyev")
+name="Mahkam"
+age=21
+print(name)
+print(age)
+print(f"my name is {name}")
+print("Hello \tworld")
+print("hello \nworld")
+name=input("What is yoour name?\n>>>")
+print("Hello,"+ name.title())
+temp=36.6
+aholi_soni=7_594_895_255
+print(aholi_soni)
+PI=3.14159
 cars=[]
 numbers=[1200, 789, 159, 21, 202]
 cars.append("Tracker")
