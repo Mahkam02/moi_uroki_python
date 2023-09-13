2509f672baf1dc529f293584c2d7359704dfdb32
Mahkam02 <mahkamkenzaboev@gmail.com>
2023-09-13 22:03:30 +0300

    hello_world

---
+++ my_first_program.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  1 20:15:22 2023
+
+@author: Mahkam
+"""
+
+#print("Hello world")
+#print(18*5-37)
+#print("hello \"world\" from pythonist")
+#print("""my name is Mahkam 
+#      I'm 21 years old""")
+#print("Mahkam \nKenjaboyev")
+#name="Mahkam"
+#age=21
+#print(name)
+#print(age)
+#print(f"my name is {name}")
+#print("Hello \tworld")
+#print("hello \nworld")
+#name=input("What is yoour name?\n>>>")
+#print("Hello,"+ name.title())
+#temp=36.6
+#aholi_soni=7_594_895_255
+#print(aholi_soni)
+#PI=3.14159
+cars=[]
+numbers=[1200, 789, 159, 21, 202]
+cars.append("Tracker")
+cars.append("Malibu")
+cars.append("Gentra")
+numbers[2]="a lot of"
+cars.insert(2, "Lacetti")
+del numbers[-1]
+numbers.append(1200)
+cars.remove("Malibu")
+numbers.remove(1200)
+a=numbers.pop(2)
+a1=numbers.pop()
